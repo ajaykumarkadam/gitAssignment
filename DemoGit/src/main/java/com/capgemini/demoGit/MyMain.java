c5a621e6c3d53fd1ccbfc3a0055d2216cf16542d
lnc <lnc@DIN76000546.corp.capgemini.com>
2016-10-19 17:22:51 +0530

    Second Commit

--- DemoGit/src/main/java/com/capgemini/demoGit/MyMain.java
+++ DemoGit/src/main/java/com/capgemini/demoGit/MyMain.java
@@ -4,7 +4,9 @@ public class MyMain {
 
 	public static void main(String[] args) {
 	System.out.println("This main Method");
+	System.out.println("This is changed in first Branch");
 	
+	System.out.println("sadmfhakhn");
 	}
 
 }
